72f5ec6570f3d8a53e5ca2a423042f49202c6389
alecschley <aschley@uwm.edu>
2019-09-24 15:43:41 -0500

    test

--- test_rational.py
+++ test_rational.py
@@ -3,12 +3,25 @@ from unittest import TestCase
 from Lab4 import Rational
 
 class TestRational(TestCase):
-    def test_setup(self):
-        self.rational = Rational()
+    def test_setUp(self):
+        self.rational = Rational(1, 2)
 
-class TestInit(TestRational):
-    def test_initial(self):
-        self.rational.__init__(self, 1, 2)
-
+class TestAdd(TestRational):
     def test_zero(self):
-        self.rational.__init__(self, 0, 0)
+        self.rational.__init__(0, 1)
+        self.assertEqual(Rational().__init__(0, 1), self.rational.__add__(self, 0))
+    def test_one(self):
+        self.rational.__init__(1, 1)
+        self.assertEqual(Rational().__init__(0, 1), self.rational.__add__(self, 0))
+    def test_oneHalf(self):
+        self.rational.__init__(1, 2)
+        self.assertEqual(Rational().__init__(1, 2), self.rational.__add__(self, 0))
+    def test_ten(self):
+        self.rational.__init__(5, 1)
+        self.assertEqual(Rational().__init__(5, 1), self.rational.__add__(self, 5))
+    def test_negativeOne(self):
+        self.rational.__init__(-1, 1)
+        self.assertEqual(Rational().__init__(-1, 1), self.rational.__add__(self, 0))
+    def test_oneHundred(self):
+        self.rational.__init__(50, 2)
+        self.assertEqual(Rational().__init__(100, 1), self.rational.__add__(self, 75))

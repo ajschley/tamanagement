2d7ec48d5d5ef3026648ff8a84ab09ee6d0f620c
Simon <sgwade@uwm.edu>
2019-09-24 17:22:09 -0500

    Working Test cases for Add(), Sub(), Mul(), and Div()

--- test_rational.py
+++ test_rational.py
@@ -1,28 +1,77 @@
+import unittest
 from unittest import TestCase
 
 from Lab4 import Rational
 
-class TestRational(TestCase):
+
+class TestRational(unittest.TestCase):
     def setUp(self):
-        self.rational = Rational(0, 1)
+        self.rational = Rational()
+
+
+class TestAdd(TestRational):
+    def test_zero(self):
+        self.rational.__init__(2, 1)
+        self.rational.__mul__(0)
+        self.assertTrue(self.rational.n == 2)
+
+    def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__mul__(-1)
+        self.assertTrue(self.rational.n == 1)
+
+    def test_number(self):
+        self.rational.__init__(5, 1)
+        self.rational.__mul__(5)
+        self.assertTrue(self.rational.n == 10)
+
+
+class TestSub(TestRational):
+    def test_zero(self):
+        self.rational.__init__(2, 1)
+        self.rational.__sub__(0)
+        self.assertTrue(self.rational.n == 2)
+
+    def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__sub__(-1)
+        self.assertTrue(self.rational.n == 3)
+
+    def test_number(self):
+        self.rational.__init__(5, 1)
+        self.rational.__sub__(5)
+        self.assertTrue(self.rational.n == 0)
+
 
 class TestMul(TestRational):
     def test_zero(self):
-        self.rational.__init__(0, 1)
-        self.assertEqual(Rational().__init__(0, 1), self.rational.__mul__(0))
-    def test_zero2(self):
-        self.rational.__init__(5, 1)
-        self.assertEqual(Rational().__init__(0, 1), self.rational.__mul__(0))
-    def test_twentyFive(self):
-        self.rational.__init__(5, 1)
-        self.assertEqual(Rational().__init__(25, 1), self.rational.__mul__(5))
+        self.rational.__init__(2, 1)
+        self.rational.__mul__(0)
+        self.assertTrue(0 == self.rational.n)
+
     def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__mul__(-1)
+        self.assertTrue(self.rational.n > 0)
+
+    def test_number(self):
         self.rational.__init__(5, 1)
-        self.assertEqual(Rational().__init__(-25, 1), self.rational.__mul__(-5))
+        self.rational.__mul__(5)
+        self.assertTrue(self.rational.n == 25)
+
+
 class TestDiv(TestRational):
-    def test_five(self):
-        self.rational.__init__(25, 1)
-        self.assertEqual(Rational().__init__(5, 1), self.rational.__div__(5))
-    def test_same(self):
-        self.rational.__init__(25, 1)
-        self.assertEqual(Rational().__init__(25, 1), self.rational.__div__(1))
+    def test_zero(self):
+        self.rational.__init__(1, 1)
+        self.rational.__div__(0)
+        self.assertTrue(1 == self.rational.n)
+
+    def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__div__(-1)
+        self.assertTrue(self.rational.n > 0)
+
+    def test_number(self):
+        self.rational.__init__(5, 1)
+        self.rational.__mul__(5)
+        self.assertTrue(self.rational.n == 1)

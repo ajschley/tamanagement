6294a083530520dbeaf41d00db6a4df7eda440a1
Chris Linske <cwlinske@uwm.edu>
2019-09-26 10:08:46 -0500

    Added all test cases, removed old files

--- test_rational.py
+++ test_rational.py
@@ -9,10 +9,78 @@ class TestRational(TestCase):
 
 class TestInit(TestRational):
     def test_initial(self):
-        self.rational.__init__(1, 2)
+        self.rational.__init__(self, 1, 2)
 
     def test_zero(self):
+        self.rational.__init__(self, 0, 0)
+
+
+class TestAdd(TestRational):
+    def test_zero(self):
+        self.rational.__init__(2, 1)
+        self.rational.__add__(0)
+        self.assertTrue(self.rational.n == 2)
+
+    def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__add__(-1)
+        self.assertTrue(self.rational.n == 1)
+
+    def test_number(self):
+        self.rational.__init__(5, 1)
+        self.rational.__add__(5)
+        self.assertTrue(self.rational.n == 10)
+
+
+class TestSub(TestRational):
+    def test_zero(self):
+        self.rational.__init__(2, 1)
+        self.rational.__sub__(0)
+        self.assertTrue(self.rational.n == 2)
+
+    def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__sub__(-1)
+        self.assertTrue(self.rational.n == 3)
+
+    def test_number(self):
+        self.rational.__init__(5, 1)
+        self.rational.__sub__(5)
+        self.assertTrue(self.rational.n == 0)
+
+
+class TestMul(TestRational):
+    def test_zero(self):
+        self.rational.__init__(2, 1)
         self.rational.__mul__(0)
+        self.assertTrue(0 == self.rational.n)
+
+    def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__mul__(-1)
+        self.assertTrue(self.rational.n > 0)
+
+    def test_number(self):
+        self.rational.__init__(5, 1)
+        self.rational.__mul__(5)
+        self.assertTrue(self.rational.n == 25)
+
+
+class TestDiv(TestRational):
+    def test_zero(self):
+        self.rational.__init__(1, 1)
+        self.rational.__div__(0)
+        self.assertTrue(1 == self.rational.n)
+
+    def test_negative(self):
+        self.rational.__init__(2, 1)
+        self.rational.__div__(-1)
+        self.assertTrue(self.rational.n > 0)
+
+    def test_number(self):
+        self.rational.__init__(5, 1)
+        self.rational.__div__(5)
+        self.assertTrue(self.rational.n == 1)
 
 
 class TestStr(TestRational):
@@ -27,3 +95,20 @@ class TestStr(TestRational):
         self.rational.__init__(1, 5)
         self.assertEqual("1/5", self.rational.__str__())
 
+
+class TestFloat(TestRational):
+    def test_int(self):
+        self.rational.__init__(10, 2)
+        self.assertEqual(5.0, self.rational.__float__())
+
+    def test_float_int(self):
+        self.rational.__init__(10.0, 2)
+        self.assertEqual(5.0, self.rational.__float__())
+
+    def test_int_float(self):
+        self.rational.__init__(10, 2.0)
+        self.assertEqual(5.0, self.rational.__float__())
+
+    def test_float_float(self):
+        self.rational.__init__(10.0, 2.0)
+        self.assertEqual(5.0, self.rational.__float__())

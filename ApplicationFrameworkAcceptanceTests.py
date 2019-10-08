8e9bb187f2c35a5794ed1cfde7222d927502d404
Simon <sgwade@uwm.edu>
2019-10-08 15:04:29 -0500

    More Login Test Cases

--- ApplicationFrameworkAcceptanceTests.py
+++ ApplicationFrameworkAcceptanceTests.py
@@ -12,12 +12,12 @@ class TestLogin(TestApplicationFramework) :
     def test_login(self):
         self.fail()
 
-    # Fails if username is not a valid UWM email address
+    # Fails if the username is not a valid UWM email address
     def test_loginUWMPass(self):
         self.ApplicationFramework.__login__("test@gmail.com", "password")
         self.assertEquals(str, "Login Failed.")
 
-    # Passes if username is a valid UWM email address
+    # Passes if the username is a valid UWM email address
     def test_loginUWMFail(self):
         self.ApplicationFramework.__login__("test@uwm.edu", "password")
         self.assertEquals(str, "Login is Successful.")

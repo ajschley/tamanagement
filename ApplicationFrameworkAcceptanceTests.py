25e87f508b0d0bdac1fd60d20861477ea318182a
Simon <sgwade@uwm.edu>
2019-10-08 15:03:34 -0500

    Working Test cases for Add(), Sub(), Mul(), and Div()

--- ApplicationFrameworkAcceptanceTests.py
+++ ApplicationFrameworkAcceptanceTests.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 from ApplicationFramework import ApplicationFramework
 
@@ -11,3 +12,16 @@ class TestLogin(TestApplicationFramework) :
     def test_login(self):
         self.fail()
 
+    # Fails if username is not a valid UWM email address
+    def test_loginUWMPass(self):
+        self.ApplicationFramework.__login__("test@gmail.com", "password")
+        self.assertEquals(str, "Login Failed.")
+
+    # Passes if username is a valid UWM email address
+    def test_loginUWMFail(self):
+        self.ApplicationFramework.__login__("test@uwm.edu", "password")
+        self.assertEquals(str, "Login is Successful.")
+
+
+if __name__ == '__main__':
+    unittest.main()

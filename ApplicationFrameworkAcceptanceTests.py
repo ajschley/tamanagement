e07b910c44b1df3d62d2d50fcb69a74e798c069d
Chris Linske <cwlinske@uwm.edu>
2019-10-08 14:32:39 -0500

    Lab6 Wrote initial acceptance test framework

---
+++ ApplicationFrameworkAcceptanceTests.py
@@ -0,0 +1,34 @@
+class ApplicationFrameworkAcceptanceTests:
+   """
+   Initial acceptance tests for the project
+   """
+   def __init__ ( self, a=0, b=1):
+      return str
+   def login (self, user, password):
+      return str
+   def createCourse (self, name):
+      return str
+   def createUser (self, name):
+      return str
+   def deleteUser (self, name):
+      return str
+   def editUser (self, name):
+      return str
+   def listClasses ( self ):
+      return str
+   def listUsers (self):
+      return str
+   def viewSelf(self):
+      return str
+   def viewProfile(self, name):
+      return str
+   def editSchedule(self):
+      return str
+   def editPreferences(self):
+      return str
+   def editResume(self):
+      return str
+   def sendEmail(self, name):
+      return str
+   def assignClass(self, user, course):
+      return str

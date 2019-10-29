2e70e409dce75aa40da4a34626297f942fe9b106
Simon <sgwade@uwm.edu>
2019-10-29 12:42:51 -0500

    Added Login/Logout functionality

--- TAmanagement/TAmanage/models.py
+++ TAmanagement/TAmanage/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# Global variable to determine if user is logged in or not
+loggedIn = False
+
 
 # Create your models here.
 class Course(models.Model):
@@ -8,10 +11,14 @@ class Course(models.Model):
 
     # Return if the class is full for TA's & Graders or not
     def isfull(self):
+        if not self.loggedIn:
+            return 'Not logged in. Cannot run function.'
         pass
 
     # Return the course name
     def coursename(self):
+        if not self.loggedIn:
+            return 'Not logged in. Cannot run function.'
         pass
 
 
@@ -25,14 +32,36 @@ class User(models.Model):
     ]
     user_type = models.CharField(max_length=5, choices=USER_TYPES, default='TA')
 
+    # User login
+    def login(self, username, password):
+        if username == self.userEmail and password == self.userPassword:
+            self.loggedIn = True
+            return 'Login successful.'
+        else:
+            return 'Login failed'
+
+    # User logout
+    def logout(self):
+        if not self.loggedIn:
+            return 'Not logged in. Cannot logout.'
+        else:
+            self.loggedIn = False
+            return 'Logout successful.'
+
     # Return the user's email/username
     def username(self):
+        if not self.loggedIn:
+            return 'Not logged in. Cannot run function.'
         pass
 
     # Reset the user's password
     def resetpassword(self):
+        if not self.loggedIn:
+            return 'Not logged in. Cannot run function.'
         pass
 
     # Return the user's type
     def usertype(self):
+        if not self.loggedIn:
+            return 'Not logged in. Cannot run function.'
         pass

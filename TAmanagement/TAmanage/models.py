cafcaef48f38415678276b6c45a1f5211427895d
Simon <sgwade@uwm.edu>
2019-10-29 12:13:45 -0500

    Django files and Slight Model Changes

--- TAmanagement/TAmanage/models.py
+++ TAmanagement/TAmanage/models.py
@@ -4,8 +4,10 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     courseName = models.CharField(max_length=29, null=False, default='CS361')
+    isFull = models.BooleanField(default=False)
 
 
 class User(models.Model):
     userEmail = models.CharField(max_length=50, null=False)
     userPassword = models.CharField(max_length=100, null=False)
+    isAdmin = models.BooleanField(default=False)
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Spider\Documents\red-dragon-cs-361\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

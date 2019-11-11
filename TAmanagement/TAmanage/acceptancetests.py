8558c064f3a6e53e447c7bf35d1ce9feef64f40b
alecschley <aschley@uwm.edu>
2019-11-10 23:14:58 -0600

    added more acceptance tests

--- TAmanagement/TAmanage/acceptancetests.py
+++ TAmanagement/TAmanage/acceptancetests.py
@@ -4,6 +4,18 @@ from .models import User
 from .commands import CommandWorker
 
 
+class WorkerTest(TestCase):
+
+    def setUp(self):
+        self.worker = CommandWorker()
+        self.admin = User.objects.create(email='admin@test.com', role=3)
+        self.worker.currentUser = self.admin
+
+    def test_worker(self):
+        msg = self.worker.executeCommand("adsfasdfsdf")
+        self.assertEqual(msg, "Not a valid command")
+
+
 class CreateCourseTest(TestCase):
 
     def setUp(self):
@@ -13,11 +25,35 @@ class CreateCourseTest(TestCase):
 
     def test_create_a_course(self):
         course = Course.objects.filter(name='CS999')
+        self.assertEqual(course.count(), 0)
+        msg = self.worker.executeCommand("create course CS999")
+        self.assertEqual(msg, 'Course added')
+        self.assertEqual(course.count(), 1)
+
+    def test_create_a_course_1(self):
+        course = Course.objects.filter(name='CS999')
+        self.assertEquals(course.count(), 0)
+        msg = self.worker.executeCommand("create course CS999 asdf")
+        self.assertEqual(msg, 'Invalid number of parameters')
+        self.assertEqual(course.count(), 0)
+
+    def test_create_a_course_2(self):
+        course = Course.objects.filter(name='CS999')
+        self.assertEqual(course.count(), 0)
+        msg = self.worker.executeCommand("create course")
+        self.assertEqual(msg, 'Invalid number of parameters')
+        self.assertEqual(course.count(), 0)
+
+    def test_create_a_course_3(self):
+        course = Course.objects.filter(name='CS999')
         self.assertEquals(course.count(), 0)
         msg = self.worker.executeCommand("create course CS999")
         self.assertEqual(msg, 'Course added')
-        course = Course.objects.filter(name='CS999')
-        self.assertEquals(course.count(), 1)
+        self.assertEqual(course.count(), 1)
+        msg = self.worker.executeCommand("create course CS999")
+        self.assertEqual(msg, 'Course already exists')
+        self.assertEqual(course.count(), 1)
+
 
 class LoginTest(TestCase):
     def setUp(self):
@@ -26,8 +62,12 @@ class LoginTest(TestCase):
         self.worker.currentUser = self.admin
 
     def test_login(self):
-        msg = self.worker.executeCommand("login 'admin@example.com' 'so_very_simple'")
-        self.assertEqual("Logged in as admin@example.com", msg)
+        msg = self.worker.executeCommand("login admin@example.com so_very_simple")
+        self.assertEqual("Given email does not belong to an existing user", msg)
+
+    def test_login_1(self):
+        msg = self.worker.executeCommand("login admin@test.com so_very_simple")
+        self.assertEqual("Logged in as admin@test.com", msg)
 
 
 class CreateUserTest(TestCase):
@@ -40,20 +80,21 @@ class CreateUserTest(TestCase):
 
     def test_create_a_user(self):
         u = User.objects.filter(email="user@uwm.edu")
-        self.assertEquals(u.count(), 0)
+        self.assertEqual(u.count(), 0)
         msg = self.worker.executeCommand("create user 'user@uwm.edu' 'pickles4breakfast'")
         self.assertEqual(msg, 'User added')
         u = User.objects.filter(email='user@uwm.edu')
-        self.assertEquals(u.count(), 1)
+        self.assertEqual(u.count(), 1)
 
     def test_create_a_user_as_ta(self):
         self.worker.currentUser = self.ta
         u = User.objects.filter(email="user2@uwm.edu")
-        self.assertEquals(u.count(), 0)
+        self.assertEqual(u.count(), 0)
         msg = self.worker.executeCommand("create user 'user2@uwm.edu' 'pickles4breakfast'")
         self.assertEqual(msg, 'Only an Administrator can create a user')
         u = User.objects.filter(email='user2@uwm.edu')
-        self.assertEquals(u.count(), 0)
+        self.assertEqual(u.count(), 0)
+
 
 class ListUserTest(TestCase):
 
@@ -68,12 +109,12 @@ class ListUserTest(TestCase):
         u = User.objects.filter(email="ta@uwm.edu")
         User.delete(u)
         u = User.objects.all()
-        self.assertEquals(u.count(), 2)
+        self.assertEqual(u.count(), 2)
         msg = self.worker.executeCommand("list users")
-        self.assertEquals(msg, "")
+        self.assertEqual(msg, "")
 
     def test_list_users_1(self):
         u = User.objects.all()
-        self.assertEquals(u.count(), 3)
+        self.assertEqual(u.count(), 3)
         msg = self.worker.executeCommand("list users")
         self.assertEqual(msg, "")
